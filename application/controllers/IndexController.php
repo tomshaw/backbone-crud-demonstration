08c7ab096270ac47724db549b6f45032351fd6bc
Tom Shaw <tom@tomshaw.info>
2012-12-09 21:18:31 -0600

    controller soft tabs

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -2,53 +2,53 @@
 
 class IndexController extends Zend_Controller_Action
 {
-	public $_page;
-	
+    public $_page;
+    
     public function init()
     {
-    	$request = $this->getRequest();
-    	 
-    	$this->_page = $request->getParam('page', '1');
+        $request = $this->getRequest();
+        
+        $this->_page = $request->getParam('page', '1');
     }
-
+    
     public function indexAction()
     {
-    	$this->addScript('app');
+        $this->addScript('app');
     }
     
     public function customersAction()
     {
-    	$this->norender();
-    	
-    	$this->addScript('app');
-    
-    	$model = new Model_Customer();
-    	
-    	$select = $model->getCustomers();
-    	
-    	$paginator = Zend_Paginator::factory($select);
-    	
-    	$paginator->setCurrentPageNumber($this->_page);
-    	
-    	$paginator->setItemCountPerPage(10);
-    	
-    	$response = array();
-    	$response['items'] = $paginator->getIterator()->toArray();
-    	$response['pages'] = get_object_vars($paginator->getPages('Jumping'));
-    	 
-    	$this->getResponse()->setHttpResponseCode(200)->appendBody(Zend_Json::encode($response));
+        $this->norender();
+        
+        $this->addScript('app');
+        
+        $model = new Model_Customer();
+        
+        $select = $model->getCustomers();
+        
+        $paginator = Zend_Paginator::factory($select);
+        
+        $paginator->setCurrentPageNumber($this->_page);
+        
+        $paginator->setItemCountPerPage(10);
+        
+        $response          = array();
+        $response['items'] = $paginator->getIterator()->toArray();
+        $response['pages'] = get_object_vars($paginator->getPages('Jumping'));
+        
+        $this->getResponse()->setHttpResponseCode(200)->appendBody(Zend_Json::encode($response));
     }
     
     private function addScript($javascriptFile)
     {
-    	$scripts = $this->view->inlineScript();
-    	$scripts->appendFile('/javascripts/' . $javascriptFile . '.js');
+        $scripts = $this->view->inlineScript();
+        $scripts->appendFile('/javascripts/' . $javascriptFile . '.js');
     }
-
+    
     private function norender()
     {
-    	$this->_helper->viewRenderer->setNoRender();
-    	$this->_helper->getHelper('layout')->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+        $this->_helper->getHelper('layout')->disableLayout();
     }
 }
 
