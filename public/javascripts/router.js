7699c801e04d6297e50759b1e6f8a89512f70e55
Tom Shaw <tom@tomshaw.info>
2012-12-27 23:11:21 -0600

    soft tab router.js

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -1,180 +1,180 @@
 define('Router', [
-  'jquery',
-  'underscore',
-  'bootstrap',
-  'utils',
-  'backbone',
-  'HeaderView',
-  'UserListCollection',
-  'UserListView',
-  'UserAddView',
-  'UserEditView',
-  'UserAdd',
-  'User',
-  'UserModalView',
-  'UserReviewView',
-  'ProfilerView'
-], function($, _, bootstrap, utils, Backbone, HeaderView, UserListCollection, UserListView, UserAddView, UserEditView, UserAdd, User, UserModalView, UserReviewView, ProfilerView) {
-	
-	var AppRouter = Backbone.Router.extend({
-	
-	    page: 1,
-	
-	    routes: {
-	        "": "home",
-	        "index/page/:page": "pages",
-	        "index/letter/:letter": "letters",
-	        "index/page/:page/order/:order/sort/:sort": "sorting",
-	        "index/add": "addUser",
-	        "index/edit/id/:id": "editUser",
-	        "index/view/id/:id": "viewUser"
-	    },
-	
-	    initialize: function () {
-	        this.headerView = new HeaderView();
-	        this.headerView.render();
-	    },
-	
-	    home: function () {
-	        var userListCollection = new UserListCollection();
-	        userListCollection.fetch({
-	            success: function (resp) {
-	                var page = resp.pages.current;
-	                $("#content").html(new UserListView({
-	                    model: userListCollection,
-	                    page: page
-	                }).render().el);
-	                utils.gridLoader();
-	            }
-	        });
-	        this.headerView.menuItem('home-menu');
-	    },
-	
-	    pages: function (page) {
-	        var page = page ? parseInt(page, 10) : 1;
-	        var userListCollection = new UserListCollection();
-	        userListCollection.fetch({
-	            data: {
-	                page: page
-	            },
-	            success: function (resp) {
-	                $("#content").html(new UserListView({
-	                    model: userListCollection,
-	                    page: page
-	                }).render().el);
-	                utils.gridLoader();
-	            }
-	        });
-	        this.headerView.menuItem('home-menu');
-	        this.page = page;
-	    },
-	
-	    letters: function (letter) {
-	        var letter = letter ? letter : '';
-	        var userListCollection = new UserListCollection();
-	        userListCollection.fetch({
-	            data: {
-	                letter: letter
-	            },
-	            success: function (resp) {
-	                $("#content").html(new UserListView({
-	                    model: userListCollection,
-	                    letter: letter
-	                }).render().el);
-	                utils.gridLoader();
-	            }
-	        });
-	        this.headerView.menuItem('home-menu');
-	    },
-	
-	    sorting: function (page, order, sort) {
-	        var page = page ? parseInt(page, 10) : 1;
-	        var userListCollection = new UserListCollection();
-	        userListCollection.fetch({
-	            data: {
-	                page: page,
-	                order: order,
-	                sort: sort
-	            },
-	            success: function (resp) {
-	                $("#content").html(new UserListView({
-	                    model: userListCollection,
-	                    page: page,
-	                    order: order,
-	                    sort: sort
-	                }).render().el);
-	                utils.gridLoader();
-	            }
-	        });
-	        this.headerView.menuItem('home-menu');
-	        this.page = page;
-	    },
-	
-	    addUser: function () {
-	        var user = new UserAdd();
-	        $('#content').append(new UserAddView({
-	            model: user
-	        }).render().el);
-	        $("#user-form-modal").modal({
-	            show: true,
-	            backdrop: true,
-	            keyboard: true
-	        });
-	        $("#user-form-modal").show();
-	        $("#modal-header").html("Add New User");
-	        utils.hideAlert();
-	        this.headerView.menuItem('add-menu');
-	    },
-	
-	    editUser: function (id) {
-	        var user = new User({
-	            id: id
-	        });
-	        var currentPage = this.page;
-	        user.fetch({
-	            success: function (resp) {
-	                $(document.body).append(new UserModalView().el);
-	                $('#user-modal-body').html(new UserEditView({
-	                    model: user,
-	                    page: currentPage
-	                }).render().el);
-	                $("#user-form-modal").modal({
-	                    show: true,
-	                    backdrop: true,
-	                    keyboard: true
-	                });
-	                $("#user-form-modal").show();
-	                $("#modal-header").html("Edit User");
-	                try {
-	                    $('#created').datepicker({
-	                        dateFormat: 'yy-mm-dd'
-	                    });
-	                } catch (error) {
-	                    if (console) console.log('datepicker error: ' + error.message);
-	                }
-	            }
-	        });
-	        this.headerView.menuItem('home-menu');
-	    },
-	
-	    viewUser: function (id) {
-	        var user = new User({
-	            id: id
-	        });
-	        user.fetch({
-	            success: function (resp) {
-	                $("#content").html(new UserReviewView({
-	                    model: user
-	                }).el);
-	                $("#profiler").html(new ProfilerView({
-	                    profiler: resp.attributes.profiler
-	                }).render().el);
-	            }
-	        });
-	        this.headerView.menuItem('home-menu');
-	    }
-	
-	});
+    'jquery',
+    'underscore',
+    'bootstrap',
+    'utils',
+    'backbone',
+    'HeaderView',
+    'UserListCollection',
+    'UserListView',
+    'UserAddView',
+    'UserEditView',
+    'UserAdd',
+    'User',
+    'UserModalView',
+    'UserReviewView',
+    'ProfilerView'
+], function ($, _, bootstrap, utils, Backbone, HeaderView, UserListCollection, UserListView, UserAddView, UserEditView, UserAdd, User, UserModalView, UserReviewView, ProfilerView) {
+
+    var AppRouter = Backbone.Router.extend({
+
+        page: 1,
+
+        routes: {
+            "": "home",
+            "index/page/:page": "pages",
+            "index/letter/:letter": "letters",
+            "index/page/:page/order/:order/sort/:sort": "sorting",
+            "index/add": "addUser",
+            "index/edit/id/:id": "editUser",
+            "index/view/id/:id": "viewUser"
+        },
+
+        initialize: function () {
+            this.headerView = new HeaderView();
+            this.headerView.render();
+        },
+
+        home: function () {
+            var userListCollection = new UserListCollection();
+            userListCollection.fetch({
+                success: function (resp) {
+                    var page = resp.pages.current;
+                    $("#content").html(new UserListView({
+                        model: userListCollection,
+                        page: page
+                    }).render().el);
+                    utils.gridLoader();
+                }
+            });
+            this.headerView.menuItem('home-menu');
+        },
+
+        pages: function (page) {
+            var page = page ? parseInt(page, 10) : 1;
+            var userListCollection = new UserListCollection();
+            userListCollection.fetch({
+                data: {
+                    page: page
+                },
+                success: function (resp) {
+                    $("#content").html(new UserListView({
+                        model: userListCollection,
+                        page: page
+                    }).render().el);
+                    utils.gridLoader();
+                }
+            });
+            this.headerView.menuItem('home-menu');
+            this.page = page;
+        },
+
+        letters: function (letter) {
+            var letter = letter ? letter : '';
+            var userListCollection = new UserListCollection();
+            userListCollection.fetch({
+                data: {
+                    letter: letter
+                },
+                success: function (resp) {
+                    $("#content").html(new UserListView({
+                        model: userListCollection,
+                        letter: letter
+                    }).render().el);
+                    utils.gridLoader();
+                }
+            });
+            this.headerView.menuItem('home-menu');
+        },
+
+        sorting: function (page, order, sort) {
+            var page = page ? parseInt(page, 10) : 1;
+            var userListCollection = new UserListCollection();
+            userListCollection.fetch({
+                data: {
+                    page: page,
+                    order: order,
+                    sort: sort
+                },
+                success: function (resp) {
+                    $("#content").html(new UserListView({
+                        model: userListCollection,
+                        page: page,
+                        order: order,
+                        sort: sort
+                    }).render().el);
+                    utils.gridLoader();
+                }
+            });
+            this.headerView.menuItem('home-menu');
+            this.page = page;
+        },
+
+        addUser: function () {
+            var user = new UserAdd();
+            $('#content').append(new UserAddView({
+                model: user
+            }).render().el);
+            $("#user-form-modal").modal({
+                show: true,
+                backdrop: true,
+                keyboard: true
+            });
+            $("#user-form-modal").show();
+            $("#modal-header").html("Add New User");
+            utils.hideAlert();
+            this.headerView.menuItem('add-menu');
+        },
+
+        editUser: function (id) {
+            var user = new User({
+                id: id
+            });
+            var currentPage = this.page;
+            user.fetch({
+                success: function (resp) {
+                    $(document.body).append(new UserModalView().el);
+                    $('#user-modal-body').html(new UserEditView({
+                        model: user,
+                        page: currentPage
+                    }).render().el);
+                    $("#user-form-modal").modal({
+                        show: true,
+                        backdrop: true,
+                        keyboard: true
+                    });
+                    $("#user-form-modal").show();
+                    $("#modal-header").html("Edit User");
+                    try {
+                        $('#created').datepicker({
+                            dateFormat: 'yy-mm-dd'
+                        });
+                    } catch (error) {
+                        if (console) console.log('datepicker error: ' + error.message);
+                    }
+                }
+            });
+            this.headerView.menuItem('home-menu');
+        },
+
+        viewUser: function (id) {
+            var user = new User({
+                id: id
+            });
+            user.fetch({
+                success: function (resp) {
+                    $("#content").html(new UserReviewView({
+                        model: user
+                    }).el);
+                    $("#profiler").html(new ProfilerView({
+                        profiler: resp.attributes.profiler
+                    }).render().el);
+                }
+            });
+            this.headerView.menuItem('home-menu');
+        }
+
+    });
 
     return AppRouter;
 });
