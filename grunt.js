6d86baa35a2f5afa51538b27f2c59f5eb0523b7b
Tom Shaw <tom@tomshaw.info>
2013-01-13 00:58:35 -0600

    Grunt file testing.

--- grunt.js
+++ grunt.js
@@ -1,21 +1,48 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
+		pkg : '<json:package.json>',
 		lint : {
 			all : [ 'grunt.js', 'public/javascripts/models/*.js',
 					'public/javascripts/collections/*.js',
 					'public/javascripts/views/*.js',
 					'public/javascripts/routers/*.js' ]
 		},
-		jshint : {
-			options : {
-				browser : true
-			}
-		},
 		jasmine : {
 			amd : true,
 			specs : 'public/javascripts/tests/spec/*.js',
-			helpers : [ 'public/javascripts/lib/require-2.1.2.js', 'public/javascripts/tests/SpecRunner.js' ]
+			helpers : [ 'public/javascripts/lib/require-2.1.2.js',
+					'public/javascripts/tests/SpecRunner.js' ]
+		},
+		watch : {
+			files : '<config:lint.files>',
+			tasks : 'default'
+		},
+		jshint : {
+			options : {
+				browser : true,
+				curly : true,
+				eqeqeq : true,
+				immed : true,
+				latedef : true,
+				newcap : true,
+				noarg : true,
+				sub : true,
+				undef : true,
+				boss : true,
+				eqnull : true,
+				node : true,
+				es5 : true,
+				globals : {
+					exports : true,
+					require : true,
+					module : true
+				},
+			},
+			all : [ 'grunt.js', 'public/javascripts/models/*.js',
+					'public/javascripts/collections/*.js',
+					'public/javascripts/views/*.js',
+					'public/javascripts/routers/*.js' ]
 		}
 	});
 
