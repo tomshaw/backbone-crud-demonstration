97255a4438c2aabafe3eecb91724eb3e08d6041c
Tom Shaw <tom@tomshaw.info>
2013-02-27 15:09:02 -0600

    Added tucked corners profile view.

--- tasks/mytask.js
+++ tasks/mytask.js
@@ -1,5 +1,20 @@
 module.exports = function( grunt ) {
     "use strict";
+    
+    exports.gitRecentChanges = function (callback) {
+        grunt.util.spawn({
+            cmd: "git",
+            args: [ "show", "--pretty=format:", "--name-only", "HEAD" ]
+        }, function( err, result ) {
+            if (err) {
+                return callback(err, null);
+            }
+
+            var changed = {};
+            changed = grunt.utils._.compact(result.split("\n"));
+            return callback(null, changed);
+        });
+    };
 
     grunt.registerTask("mytask", function(option) {
         var done = this.async();
@@ -8,16 +23,23 @@ module.exports = function( grunt ) {
         
         //grunt.warn('This is a cool warning.');
 
-        grunt.log.write("Running Custom MyTask!!! \n");
+        grunt.log.write("Running BBCRUD Custom Tasks \n");
         
         var pkg = require('../package.json');
         
         console.log('BBCRUD: ' + pkg.description + ' (v' + pkg.version + ')');
         console.log('');
       
-        grunt.task.run('concat min');
+        //grunt.task.run('concat min');
         
-        done();
+        exports['gitRecentChanges'](function(err, files) {
+            if (err) {
+                grunt.log.error(err);
+                return done(false);
+            }
+            console.log(files);
+            done();
+        });
 
     });
 
